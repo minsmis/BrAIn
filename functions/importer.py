9a7ab1ab9e2ad39b69fc1b36d69f8142dd8a28a5
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-12-18 01:00:26 +0900

    :sparkles: Add bin averaging for extract interaction function.

--- functions/importer.py
+++ functions/importer.py
@@ -18,9 +18,20 @@ def extract_interaction(bool_interaction, ndarr_calcium, num_sampling_frequency,
     # **kwargs
     # 'align' [Ture/False (Default)]: Reshape Epoch x Cell x Bin {3D} to (Epoch x Cell) x Bin {2D}
     # 'shuffle' [True/False (Default)]: Shuffle epochs randomly
+    # 'do_average' [True/False (Default)]: Average calcium transient within bin.
 
     def is_all_equal(array):
         return all(element == array[0] for element in array)
+
+    # Variables
+    BOOL_DO_AVERAGE = False
+
+    # Get averaging mode
+    if 'do_average' in kwargs:
+        BOOL_DO_AVERAGE = kwargs.get('do_average')
+        if not isinstance(BOOL_DO_AVERAGE, bool):
+            BOOL_DO_AVERAGE = False
+            print("Check keyword arguments: 'do_average'")
 
     # Output
     list_interaction_calcium_dataset = []
@@ -32,11 +43,14 @@ def extract_interaction(bool_interaction, ndarr_calcium, num_sampling_frequency,
     for i in np.arange(0, len(bool_interaction), BIN_LENGTH_TO_INDEX):
         bool_temp_interaction_block = bool_interaction[i:i + BIN_LENGTH_TO_INDEX]
         ndarr_temp_calcium_block = ndarr_calcium[i:i + BIN_LENGTH_TO_INDEX, :]
+        if BOOL_DO_AVERAGE is True:
+            ndarr_temp_calcium_block = np.average(ndarr_temp_calcium_block, axis=0)  # Average bin
 
         if is_all_equal(bool_temp_interaction_block) is True:
             if bool_temp_interaction_block[0] == 1:  # When interaction
+                # 1D: Interaction bout, 2D: Cell, 3D: Bin OR 1D: Interaction bout, 2D: Mean event of cell
                 list_interaction_calcium_dataset.append(
-                    np.transpose(ndarr_temp_calcium_block))  # 1D: Epoch, 2D: Cell, 3D: Bin
+                    np.transpose(ndarr_temp_calcium_block))
 
         if is_all_equal(bool_temp_interaction_block) is False:
             num_excluded_blocks += 1  # Increase # of excluded blocks
