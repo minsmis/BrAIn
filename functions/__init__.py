bafdf40f41e14c169b295b4f00397b1a050cf79d
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-12-18 01:00:49 +0900

    :sparkles: Add newly introduced functions.

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,6 +4,9 @@ from .importer import extract_interaction
 
 # functions.dataset
 from .dataset import import_dataset
+from .dataset import split_dataset
+from .dataset import feature_selection
 from .dataset import make_class
 from .dataset import make_null_dataset
 from .dataset import make_dataset
+from .dataset import make_dataset2
